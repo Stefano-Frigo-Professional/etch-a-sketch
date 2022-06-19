909f164ec2d83f4d96ade137cfd8f071da0e27ae
Stefano <sfrigouk@gmail.com>
2022-06-19 23:12:17 +0100

    Make each member of the grid go red when hovered over

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ for(let i = 0; i<257; i++){
     const newDiv = document.createElement("div");
     container.appendChild(newDiv);
 }
+
+function changeColour(e) {
+    e.target.style.borderColor = "red";
+}
+
+const containerDivs = document.querySelectorAll(".container div");
+containerDivs.forEach((containerDiv) => {
+    containerDiv.addEventListener("mouseenter", changeColour);
+});
