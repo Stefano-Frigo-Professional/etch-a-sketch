2df558a47c929c23ad3f5c74b0693c5d2ed18fd0
Stefano <sfrigouk@gmail.com>
2022-06-19 23:30:01 +0100

    Move initial grid creation code into generic function

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const container = document.querySelector(".container");
 
-for(let i = 0; i<257; i++){
-    const newDiv = document.createElement("div");
-    container.appendChild(newDiv);
+function createNewGrid(size){
+    for(let i = 0; i<Math.pow(size, 2); i++){
+        const newDiv = document.createElement("div");
+        container.appendChild(newDiv);
+    }
 }
+
+createNewGrid(16);
 
 function changeColour(e) {
     e.target.classList.add("red-square");
@@ -13,3 +17,11 @@ const containerDivs = document.querySelectorAll(".container div");
 containerDivs.forEach((containerDiv) => {
     containerDiv.addEventListener("mouseenter", changeColour);
 });
+
+const selectionButton = document.querySelector("button.selection");
+selectionButton.addEventListener("click", askForGridSize);
+
+function askForGridSize(e){
+    const size = prompt("What grid size do you want? Max 100.");
+    createNewGrid(size);
+}
